4e3d254059a0d7e4249b66ed1e3168f24596c804
metinbaris <hizmetparki@gmail.com>
2024-03-03 12:43:08 +0300

    Create draft Entity for stocks

---
+++ src/Entity/Stocks.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace MetinBaris\InventoryBundle\Entity;
+
+use MetinBaris\InventoryBundle\Repository\StocksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StocksRepository::class)]
+class Stocks
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+}
