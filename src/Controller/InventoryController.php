7fd405ce7344497509108e497cd40e50a7914d45
metinbaris <hizmetparki@gmail.com>
2024-03-05 10:48:36 +0300

    Update controller action to save
    Update form naming on views

--- src/Controller/InventoryController.php
+++ src/Controller/InventoryController.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use MetinBaris\InventoryBundle\Entity\Stocks;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InventoryController extends AbstractController
@@ -24,5 +25,32 @@ class InventoryController extends AbstractController
         return $this->render('@Inventory/InventoryController/index.html.twig', [
             'stocks' => $stocks,
         ]);
+    }
+
+    #[Route("/save-product", "save_product")]
+    public function save(Request $request): Response
+    {
+        $sku = $request->request->get('sku');
+        $quantity = $request->request->get('quantity');
+
+        if ($request->isMethod('POST')) {
+
+            $existingStock = $this->entityManager->getRepository(Stocks::class)->findOneBy(['sku' => $sku]);
+            if ($existingStock) {
+                $existingStock->setQuantity($quantity);
+                $stock = $existingStock;
+            } else {
+                $stock = new Stocks();
+                $stock->setSku($sku);
+                $stock->setQuantity($quantity);
+            }
+
+            $this->entityManager->persist($stock);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('index_route');
+        }
+
+        return $this->render('@Inventory/InventoryController/save.html.twig');
     }
 }
