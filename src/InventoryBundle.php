1246779c560279fcc7891a561b52213bb431cd31
metinbaris <hizmetparki@gmail.com>
2024-03-06 13:53:20 +0300

    Remove not needed class
    Readme file has enough explanations for how to install the bundle

--- src/InventoryBundle.php
+++ src/InventoryBundle.php
@@ -4,20 +4,12 @@ namespace MetinBaris\InventoryBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use MetinBaris\InventoryBundle\App;
 use MetinBaris\InventoryBundle\DependencyInjection\Compiler\RegisterConsoleCommandPass;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class InventoryBundle extends Bundle
 {
-    public function boot(): void
-    {
-        parent::boot();
-
-        App::init();
-    }
-
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
